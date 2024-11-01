a7f2ded727c78d80bcb950a47dafe3d77e46cb17
Pintu900 <shahpintu900@gmail.com>
2024-11-02 01:35:08 +0530

    dotenv

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 // src/supabase.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://qtdppmkhcsmjntjygvxn.supabase.co'; // Replace with your Supabase project URL
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF0ZHBwbWtoY3Ntam50anlndnhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk5NTY0MTIsImV4cCI6MjA0NTUzMjQxMn0.7XZlq07QmSOCk3-z8FtqLjhghMjJfVbpOFI43OjvkMg'; // Replace with your Supabase anon key
+console.log(process.env.VUE_APP_API_URL);
+const supabaseUrl = process.env.VUE_APP_API_URL; // Replace with your Supabase project URL
+const supabaseKey = process.env.VUE_APP_SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
